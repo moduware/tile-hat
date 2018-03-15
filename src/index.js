f61c44982954f5cd6153f9d5139a0aae37b6ae3e
Alex Chernov <alexusblack@gmail.com>
2018-03-15 11:20:08 +1100

    mdl and header integration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import 'reset-css/reset.css';
+import 'material-design-lite/material.min.css';
 import './sass/styles.scss';
+
+import 'material-design-lite/material.min.js';
+import 'webview-tile-header/WebViewTileHeader.js';
+
+import headerSettingsIcon from './img/icon-settings.svg';
+
+console.log(headerSettingsIcon);
+
+Nexpaq.Header.create('Temperature');
+Nexpaq.Header.customize({color: 'white', iconColor:'white', backgroundColor:'#FFB931', borderBottom:'none'});
+Nexpaq.Header.hideShadow();
+Nexpaq.Header.addButton({image: headerSettingsIcon}, null);
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
         }]
       },
       {
+        test: /\.css$/,
+        use: [ 
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
         test: /\.(jpg|png|svg)$/,
         use: [
           {
