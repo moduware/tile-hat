d2e40f23525f9815c8e6199b95763f8ff307e4de
Alex Chernov <alexusblack@gmail.com>
2018-03-15 12:06:14 +1100

    paging system

---
+++ src/enums/TemperatureUnit.js
@@ -0,0 +1,6 @@
+const TemperatureUnit = Object.freeze({
+    Celsius: 1,
+    Fahrenheit: 2
+});
+
+export default TemperatureUnit;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,27 @@ import 'webview-tile-header/WebViewTileHeader.js';
 
 import headerSettingsIcon from './img/icon-settings.svg';
 
+import Settings from './lib/Settings';
+import * as Pages from './lib/Pages';
+import TemperatureUnit from './enums/TemperatureUnit';
+
+const defaultSettings = {
+    units: TemperatureUnit.Celsius
+};
+Settings.setPrefix('hat');
+const settings = Settings.Load(defaultSettings);
+
 Nexpaq.Header._detectCurrentPlatform();
 Nexpaq.Header.create('Temperature');
 Nexpaq.Header.customize({color: 'white', iconColor:'white', backgroundColor:'#FFB931', borderBottom:'none'});
 Nexpaq.Header.hideShadow();
-Nexpaq.Header.addButton({image: headerSettingsIcon}, null);
+Nexpaq.Header.addButton({image: headerSettingsIcon}, () => Pages.showSettingsPage());
+Nexpaq.Header.addEventListener('BackButtonClicked', () => {
+    if(Pages.currentPage == 'main') {
+        Nexpaq.API.Exit();
+    } else {
+        Pages.showMainPage();
+    }
+});
+document.getElementById('button-snapshot').addEventListener('click', () => Pages.showSnapshotPage());
+document.getElementById('button-cancel').addEventListener('click', () => Pages.showMainPage());
