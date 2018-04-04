45e832de020bbda0af73be1dc4931e34d0920dc2
Alex Chernov <alexusblack@gmail.com>
2018-04-04 13:59:49 +1000

    zipping tile

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const merge = require('webpack-merge');
+const ZipPlugin = require('zip-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new ZipPlugin({
+            filename: 'tile.zip',
+            path: '..'
+        })
+    ]
 });
