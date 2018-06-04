64f21717e2b372c1e749d66f82ec310f26683044
Clint <clintp1@gmail.com>
2018-06-04 16:28:35 +1000

    refactor media files import

---
+++ src/lib/tabbar-icons.js
@@ -0,0 +1,22 @@
+import media from '../media';
+
+const tabbarIcons = {
+  android: {
+    tabbarResultSelected: media.tabbarResultSelectedAndroidIconSrc,
+      tabbarResultNotSelected: media.tabbarResultNotSelectedAndroidIconSrc,
+        tabbarHistorySelected: media.tabbarHistorySelectedAndroidIconSrc,
+          tabbarHistoryNotSelected: media.tabbarHistoryNotSelectedAndroidIconSrc,
+            tabbarSettingsSelected: media.tabbarSettingsSelectedAndroidIconSrc,
+              tabbarSettingsNotSelected: media.tabbarSettingsNotSelectedAndroidIconSrc
+  },
+  ios: {
+    tabbarResultSelected: media.tabbarResultSelectediOSIconSrc,
+      tabbarResultNotSelected: media.tabbarResultNotSelectediOSIconSrc,
+        tabbarHistorySelected: media.tabbarHistorySelectediOSIconSrc,
+          tabbarHistoryNotSelected: media.tabbarHistoryNotSelectediOSIconSrc,
+            tabbarSettingsSelected: media.tabbarSettingsSelectediOSIconSrc,
+              tabbarSettingsNotSelected: media.tabbarSettingsNotSelectediOSIconSrc
+  }
+};
+
+export default tabbarIcons;
